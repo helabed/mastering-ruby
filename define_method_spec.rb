c9a2b8000ab1268eea12217c00becb458ced7ff9
Hani Elabed <hani.elabed@gmail.com>
2020-08-24 13:36:00 -0500

    updates tests to RSpec 3 syntax

--- define_method_spec.rb
+++ define_method_spec.rb
@@ -7,7 +7,7 @@ describe 'define_method' do
         end
       end
       m = Multiplier.new
-      m.times_2(3).should == 6
+      expect(m.times_2(3)).to eq 6
     end
     context "it should allow us to create a simple statically named method inside a module" do
       module MultiplierFactory
@@ -20,13 +20,13 @@ describe 'define_method' do
           include MultiplierFactory
         end
         m = Multiplier.new
-        m.times_5(5).should == 25
+        expect(m.times_5(5)).to eq 25
       end
       it "can access the method when using the 'extend' keyword" do
         class MultiplierWithExtend
           extend MultiplierFactory
         end
-        MultiplierWithExtend.times_5(10).should == 50
+        expect(MultiplierWithExtend.times_5(10)).to eq 50
       end
     end
     it "should allow us to create a dynamically named method inside a class" do
@@ -39,7 +39,7 @@ describe 'define_method' do
       end
       Multiplier.create_n_times(2)
       m = Multiplier.new
-      m.times_2(3).should == 6
+      expect(m.times_2(3)).to eq 6
     end
     it "should allow us to create many dynamically named methods(i.e many similar methods) inside a class" do
       class Multiplier
@@ -53,9 +53,9 @@ describe 'define_method' do
         end
       end
       m = Multiplier.new
-      m.times_2(3).should == 6
-      m.times_5(3).should == 15
-      m.times_8(10).should == 80
+      expect(m.times_2(3)).to eq 6
+      expect(m.times_5(3)).to eq 15
+      expect(m.times_8(10)).to eq 80
     end
   end
   context "is NOT available inside a singleton object, but just for classes or modules" do
@@ -63,10 +63,10 @@ describe 'define_method' do
       class Multiplier
       end
       m = Multiplier.new
-      lambda {
+      expect {
         m.define_method(:times_2) do |val|
           val * 2
-        end}.should raise_error(NoMethodError)
+        end}.to raise_error(NoMethodError)
     end
   end
   context "can be made available inside a singleton object with the help of class_eval" do
@@ -80,7 +80,7 @@ describe 'define_method' do
           val * 2
         end
       end
-      m.times_2(3).should == 6
+      expect(m.times_2(3)).to eq 6
     end
   end
   context "can be made available inside a singleton object's ghost subclass with the help of class_eval" do
@@ -98,8 +98,8 @@ describe 'define_method' do
           val * 2
         end
       end
-      m.times_2(3).should == 6
-      m.times_3(3).should == 9
+      expect(m.times_2(3)).to eq 6
+      expect(m.times_3(3)).to eq 9
     end
   end
 end
