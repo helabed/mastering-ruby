c3e01a8de72834cac86f1d09b1581662a17f704a
Hani Elabed <hani.elabed@gmail.com>
2020-08-25 10:17:19 -0500

    updates test to RSpec 3 syntax

--- instance_eval_spec.rb
+++ instance_eval_spec.rb
@@ -1,19 +1,22 @@
 describe 'instance_eval' do
   context 'allows us to execute any code inside a block - unlike eval' do
     it "should execte any code" do
-      instance_eval do
+      result = instance_eval do
         'hello'
-      end.should == 'hello'
+      end
+      expect(result).to eq 'hello'
     end
     it "should execte any code with a receiver - unlike eval which is a private method" do
-      "cat".instance_eval do
+      result = "cat".instance_eval do
         self
-      end.should == 'cat'
+      end
+      expect(result).to eq 'cat'
     end
     it "inside the block of the receiver 'self' is set to the receiver of instance_eval" do
-      "cat".instance_eval do
+      result = "cat".instance_eval do
         upcase
-      end.should == 'CAT'
+      end
+      expect(result).to eq 'CAT'
     end
     context "allow us to cheat and access instance variables without a getter or a setter" do
       before(:all) do
@@ -31,24 +34,27 @@ describe 'instance_eval' do
         @m = MyClass.new
       end
       it "without instance_eval we should not be able to access the instance variable" do
-        lambda {@m.the_var}.should raise_error(NoMethodError)
+        expect {@m.the_var}.to raise_error(NoMethodError)
       end
-      it "with instance_eval we should be able to access the instance variable" do
-        @m.instance_eval do
+      it "with instance_eval we should be able to access the instance variable (both read/write)" do
+        result = @m.instance_eval do
           @the_var
-        end.should == 66
-        @m.instance_eval do
+        end
+        expect(result).to eq 66
+        result2 = @m.instance_eval do
           @the_var = 'the_cat'
           @the_var
-        end.should == 'the_cat'
+        end
+        expect(result2).to eq 'the_cat'
       end
       it "without instance_eval we should not be able to access private methods" do
-        lambda {@m.secret}.should raise_error(NoMethodError)
+        expect {@m.secret}.to raise_error(NoMethodError)
       end
       it "with instance_eval we should be able to access private methods - i.e methods without a receiver" do
-        @m.instance_eval do
+        result = @m.instance_eval do
           secret
-        end.should == 'my private secret'
+        end
+        expect(result).to eq 'my private secret'
       end
       it "instance_eval with a block produces a closure but not as we expect in that inside the block self is changed to the receiver of instance_eval" do
         class Other
@@ -66,8 +72,8 @@ describe 'instance_eval' do
           end
         end
         obj = Other.new
-        lambda {obj.method1(@m)}.should raise_error(NameError)
-        obj.method3(@m).should == 55
+        expect {obj.method1(@m)}.to raise_error(NameError)
+        expect(obj.method3(@m)).to eq 55
       end
     end
   end
@@ -79,11 +85,11 @@ describe 'instance_eval' do
           'miaow'
         end
       end
-      animal.speak.should == 'miaow'
+      expect(animal.speak).to eq 'miaow'
     end
     it "everytime we call instance_eval on an object, ruby creates an anonymous ghost class and places the "+
         "newly defined methods inside it, and makes it the class of the receiver of instance_eval" do
-      true.should == true
+      expect(true).to eq true
     end
     it "should allow us to define methods on the class itself (class methods) - because a class is also an object in ruby" do
       class Hani
@@ -91,32 +97,32 @@ describe 'instance_eval' do
           'hello'
         end
       end
-      Hani.speak.should == 'hello'
+      expect(Hani.speak).to eq 'hello'
       Hani.instance_eval do
         def say_goodbye
           'goodbye'
         end
       end
-      Hani.say_goodbye.should == 'goodbye'
+      expect(Hani.say_goodbye).to eq 'goodbye'
     end
     it "everytime we call instance_eval on a class, ruby creates an anonymous ghost class and places the "+
         "newly defined methods(class methods) inside it, and makes it the current class of the receiver(which is a class) of instance_eval" do
-      true.should == true
+      expect(true).to eq true
     end
     it "class_eval would declare instance methods, because class_eval can be called only on classes and modules in ruby - the name reflects the receiver" do
-      true.should == true
+      expect(true).to eq true
     end
     it "instance_eval would declare class methods, because instance_eval can be called on every single object in ruby - the name reflects the receiver" do
-      true.should == true
+      expect(true).to eq true
     end
     it "see file 'receiver_a_class__class_eval__creates_instance_methods.png' to find out what the 'def' method does inside class_eval" do
-      true.should == true
+      expect(true).to eq true
     end
     it "see file 'receiver_a_class__instance_eval__creates_class_methods.png' to find out what the 'def' method does inside instance_eval" do
-      true.should == true
+      expect(true).to eq true
     end
     it "see file 'receiver__xxx_eval__truth_table.png' to find out what the 'def' method does for all cases" do
-      true.should == true
+      expect(true).to eq true
     end
   end
   context 'allows us to create a Domain Specific Language(DSL) in a block' do
@@ -148,35 +154,35 @@ describe 'instance_eval' do
       t.right(3)
       t.up(2)
       t.right
-      t.path.should == 'rrruur'
+      expect(t.path).to eq 'rrruur'
 
       t.move do
         right(3)
         up(2)
         right
       end
-      t.path.should == 'rrruurrrruur'
+      expect(t.path).to eq 'rrruurrrruur'
 
-      lambda {
+      expect {
       t.bad_move do
         right(3)
         up(2)
         right
       end
-      }.should raise_error(NoMethodError)
+      }.to raise_error(NoMethodError)
 
       # @count below is defined in this context but not inside the to object 't' itself
       # so even though it looks like @count should be available inside the block. But remember,
       # we do NOT have a closure with instance_eval blocks, we are changing self to the receiver 't' where @count does
       # not exist.
-      lambda {
+      expect {
         @count = 3
         t.move do
           right(@count)
           up(2)
           right
         end
-      }.should raise_error(TypeError)
+      }.to raise_error(TypeError)
     end
   end
 end
