7c96216262c839c608c47c403892eced4e46caa2
Hani Elabed <hani.elabed@gmail.com>
2020-08-25 10:31:42 -0500

    updates tests with RSpec 3 syntax

--- instance_variables_spec.rb
+++ instance_variables_spec.rb
@@ -7,33 +7,30 @@ describe 'instance variables' do
     end
     it "should not allow access from the outside without a getter" do
       my_class = MyInstanceVarsClass.new(2,5)
-      lambda {
-        my_class.x}.should raise_error(NoMethodError)
+      expect {
+        my_class.x}.to raise_error(NoMethodError)
     end
     it "should allow access from the outside only when a getter is defined" do
-      MyInstanceVarsClass.class_eval { attr_reader :x }
+      MyInstanceVarsClass.class_eval { attr_reader :x } # adding getter instance method to x
       my_class = MyInstanceVarsClass.new(2,5)
-      my_class.x.should == 2
+      expect(my_class.x).to eq 2
     end
     it "should not allow modification from the outside without a setter" do
       my_class = MyInstanceVarsClass.new(2,5)
-      lambda {
-        my_class.x = 6}.should raise_error(NoMethodError)
+      expect {
+        my_class.x = 6}.to raise_error(NoMethodError)
     end
     it "should allow modification from the outside only when a setter is defined with help of class_eval and attr_writer" do
-      MyInstanceVarsClass.class_eval { attr_writer :x; attr_reader :x; }
+      MyInstanceVarsClass.class_eval { attr_writer :x; attr_reader :x; } # adding setter/getter instance methods to x
       my_class = MyInstanceVarsClass.new(2,5)
       my_class.x= 7
-      my_class.x.should == 7
+      expect(my_class.x).to eq 7
     end
     it "should allow modification from the outside only when a setter is defined with help of class_eval and attr_accessor" do
       my_class = MyInstanceVarsClass.new(2,5)
       my_class.class.class_eval { attr_accessor :x; }
       my_class.x= 8
-      my_class.x.should == 8
+      expect(my_class.x).to eq 8
     end
-
-
   end
-
 end
