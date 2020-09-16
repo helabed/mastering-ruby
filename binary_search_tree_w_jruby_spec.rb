4a819e370ba6d4d2eaf2b96ac993b1f8cfcf2a61
Hani Elabed <hani.elabed@gmail.com>
2020-09-16 10:41:13 -0500

    attempts to display BST tree with Java Swing
    with the help of JRuby. First attempt,
    moderate success.
    
    Also adds the Java class
    ConnectionsBetweenButtons.java that was the
    inspiration for drawing lines inside a JPanel

--- binary_search_tree_w_jruby_spec.rb
+++ binary_search_tree_w_jruby_spec.rb
@@ -3,7 +3,7 @@
 #require line below.
 require './binary_search_tree'
 #comment out this line for below standlone RSpec (i.e when NOT in coderpad)
-require 'rspec/autorun'
+#require 'rspec/autorun'
 ##The above statements are useful when running in
 ##coderpad.io/sandbox - otherwise comment out
 require 'securerandom'
@@ -13,7 +13,7 @@ require 'pry'
 #require 'byebug'
 
 
-RSpec.describe 'BinarySearchTree (BST) testing - iteration last' do
+RSpec.describe 'BinarySearchTree (BST) - display in JRuby/Java Swing' do
   it 'should search for an element stored in BST' do
     total_num_of_items = 15
     randomness_range = 100
@@ -33,6 +33,7 @@ RSpec.describe 'BinarySearchTree (BST) testing - iteration last' do
 
         rand_array.shuffle!
 
+        rand_array = [3,1,5,0,2]
         puts ""
         puts ""
         bst = BinarySearchTree.new
@@ -40,44 +41,91 @@ RSpec.describe 'BinarySearchTree (BST) testing - iteration last' do
 
         L.log rand_array, 'the array'
         bst.insert_into(rand_array)
+
+        # accumulator = []
+        # outcome = bst.do_ancestors_traverse(item_to_traverse_from) do |t|
+        #   accumulator << t.node.data
+        # end
+        # expect(outcome).to be true
+        #
+        # accumulator = []
+        # outcome = bst.do_descendants_traverse(item_to_traverse_from) do |t|
+        #   accumulator << t.node.data
+        # end
+        # expect(outcome).to be true
+
         bst.display_tree
 
-        L.log item_to_search_for, 'Will be searching for'
-        found, data = bst.search_for(rand_array, item_to_search_for)
-        expect(found).to be true
-        expect(data).to eq item_to_search_for
+        class Connection
+          #javax.swing.JButton source
+          attr_accessor :source
+          attr_accessor :destination
 
-        height = bst.height_of_tree
-        expect(height).to be < 30 # random high number
-
-        L.log item_to_delete, 'Will be deleting'
-        bst.display_tree
-        outcome = bst.delete_from(item_to_delete)
-        expect(outcome).to be true
-
-        bst.do_right_to_left_leaf_traverse
-        bst.do_in_order_traverse
-        bst.do_pre_in_order_traverse
-        bst.do_post_in_order_traverse
-
-        bst.do_right_to_left_leaf_traverse
-
-        accumulator = []
-        outcome = bst.do_ancestors_traverse(item_to_traverse_from) do |t|
-          accumulator << t.node.data
+          def initialize(src, dest)
+            @source      = src
+            @destination = dest
+          end
         end
-        expect(outcome).to be true
 
-        accumulator = []
-        outcome = bst.do_descendants_traverse(item_to_traverse_from) do |t|
-          accumulator << t.node.data
+        class ConnectionsPane < javax.swing.JPanel
+          attr_accessor :connections
+          attr_accessor :vertical_panel_x
+          attr_accessor :vertical_panel_y
+          attr_accessor :vertical_panel_width
+          attr_accessor :vertical_panel_height
+
+          def horizontalCenter(bounds)
+            return bounds.getX() + bounds.getWidth() / 2
+          end
+
+          def verticalCenter(bounds)
+            return bounds.getY() + bounds.getHeight() / 2
+          end
+
+          @Override
+          def paintComponent(g)
+            super(g)
+            g2d = g.create()
+            g2d.setColor(java.awt.Color::RED)
+
+            @connections.each do |c|
+              # puts "I am connection: #{c.inspect}"
+              # puts "g.inspect #{g.inspect}"
+              source = c.source
+              dest = c.destination
+
+              if source && dest
+                puts "source x: #{source.getX}, y: #{source.getY}"
+                puts "dest x: #{dest.getX}, y: #{dest.getY}"
+                if (source.getX() == dest.getX())
+                  # Same column...
+                  g2d.drawLine(source.getX() + source.getWidth() / 2, source.getY(),
+                    dest.getX() + source.getWidth() / 2, dest.getY());
+                elsif (source.getY() == dest.getY())
+                  # Same row...
+                  offset = 200
+                  g2d.drawLine(source.getX() + vertical_panel_x + offset,
+                               source.getY() + vertical_panel_y + source.getHeight() / 2 + offset,
+                               dest.getX() + vertical_panel_x + offset + 100,
+                               dest.getY() + vertical_panel_y + dest.getHeight() / 2 + offset + 100);
+                else
+                  # diagonal from each other
+                  path = new java.awt.geom.Path2D.Double();
+                  path.moveTo(horizontalCenter(source), verticalCenter(source));
+                  path.curveTo(horizontalCenter(source), verticalCenter(dest),
+                                  horizontalCenter(source), verticalCenter(dest),
+                                  horizontalCenter(dest), verticalCenter(dest));
+                  g2d.draw(path);
+                end
+              end
+            end
+            g2d.dispose();
+          end
         end
-        expect(outcome).to be true
-        bst.display_tree
-        L.log '', 'SUCCESS'
 
+        connections = []
+        buttons = {}
         levels_with_trees = BinarySearchTree::TreeDisplayer.trees_grouped_by_level(bst.root_tree)
-
         # Java swing library below in preparation for displaying trees & nodes
         # in Swing
         frame = javax.swing.JFrame.new("JRuby is Awesome!")
@@ -85,19 +133,42 @@ RSpec.describe 'BinarySearchTree (BST) testing - iteration last' do
         vertical_grid = java.awt.GridLayout.new(bst.height_of_tree, 1)
         vertical_panel = javax.swing.JPanel.new
         vertical_panel.set_layout(vertical_grid)
+        vertical_panel_border =
+          javax.swing.BorderFactory.createLineBorder(java.awt.Color::BLUE,1)
+        vertical_panel.set_border(vertical_panel_border)
         levels_with_trees.each_pair do |level, trees|
           panel = javax.swing.JPanel.new
           panel.set_layout(java.awt.FlowLayout.new)
+          panel_border =
+            javax.swing.BorderFactory.createLineBorder(java.awt.Color::GREEN,1)
+          panel.set_border(panel_border)
           trees.each_with_index do |t, i|
             button = javax.swing.JButton.new(t.node.data.to_s)
+            buttons[t.node.data] = button
+            if t.parent
+              connections << Connection.new(buttons[t.parent.node.data], button)
+            else
+              connections << Connection.new(nil, button) # when t is root
+            end
             button.add_action_listener { button.text = "L#{level}t#{i+1}" }
             panel.add button
           end
           vertical_panel.add(panel)
         end
-        frame.add(vertical_panel)
+        connection_pane = ConnectionsPane.new
+        connection_pane.connections = connections
+        connection_pane_border =
+          javax.swing.BorderFactory.createLineBorder(java.awt.Color::ORANGE,1)
+        connection_pane.set_border(connection_pane_border)
+        connection_pane.add(vertical_panel)
+        frame.add(connection_pane)
         frame.show
-
+        connection_pane.vertical_panel_x = vertical_panel.bounds.x
+        connection_pane.vertical_panel_y = vertical_panel.bounds.y
+        connection_pane.vertical_panel_width  = vertical_panel.bounds.width
+        connection_pane.vertical_panel_height = vertical_panel.bounds.height
+        puts "vertical_panel x: #{connection_pane.vertical_panel_x}, y: #{connection_pane.vertical_panel_y}"
+        #binding.pry
       end
     end
   end
